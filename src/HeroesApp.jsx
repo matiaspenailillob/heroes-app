3f53e755327279c619cd0e910fccc0f0d016d407
Matias P. <matias.penailillob@gmail.com>
2024-11-21 17:47:39 -0300

    feat: Create routes - Create AppRouter component with all routes at this point - Create empty Marvel, DC and Login pages (components) - Config in main.jsx react-router

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,8 +1,11 @@
+import { AppRouter } from "./router/AppRouter"
 
 export const HeroesApp = () => {
   return (
     <>
         <h1> Heroes App </h1>
+
+        <AppRouter />
     </>
   )
 }
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,22 @@
+//  Router Principal de la aplicación
+import { Navigate, Route, Routes } from 'react-router'
+import { MarvelPage } from '../heroes/pages/MarvelPage'
+import { DCPage } from '../heroes/pages/DCPage'
+import { LoginPage } from '../auth/pages/LoginPage'
+
+export const AppRouter = () => {
+  return (
+    <>
+        <Routes>
+
+            <Route path='marvel' element={ <MarvelPage /> } ></Route>
+            <Route path='dc' element={ <DCPage /> } ></Route>
+
+            <Route path='login' element={ <LoginPage /> } ></Route>
+
+            <Route path='/' element={ <Navigate to='/marvel' /> } ></Route>
+
+        </Routes>
+    </>
+  )
+}
