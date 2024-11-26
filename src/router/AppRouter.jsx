a5b5cf5fb4237ac4968db932fee12c4b8b8911ec
Matias P. <matias.penailillob@gmail.com>
2024-11-26 20:12:04 -0300

    feat: add public routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,19 +4,27 @@ import { LoginPage } from '../auth/pages/LoginPage'
 import { Navbar } from '../ui/components/Navbar'
 import { HeroesRoutes } from '../heroes/routes/HeroesRoutes'
 import { PrivateRoute } from './PrivateRoute'
+import { PublicRoute } from './PublicRoute'
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
-            <Route path='login' element={ <LoginPage /> } ></Route>
+
+          <Route path='login' element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          } />
+          
+            {/* <Route path='login' element={ <LoginPage /> } ></Route> */}
 
             <Route path='/*' element={
             <PrivateRoute>
               <HeroesRoutes />
             </PrivateRoute> 
           } />
-          
+
         </Routes>
     </>
   )
